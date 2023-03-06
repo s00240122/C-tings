c18738527d88a45f0542ee9b9cff32e8ddb2c235
s00240122 <s00240122@atu.ie>
2023-03-06 15:42:30 +0000

    pointers

--- Pointered/main.c
+++ Pointered/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    void foo1 (int xval);
+    void foo2 (int dummy);
+
+    foo1(7);
+    foo2(11);
+
     return 0;
+}
+
+void foo1 (int xval) {
+    int x;
+    x = xval;
+
+    printf("x.address = 0x%x\nx.value = %d\n", &x, x);
+}
+
+void foo2(int dummy)
+{
+    int y;
+    y = dummy;
+
+    printf("y.address = 0x%x\ny.value = %d\n", &y, y);
 }
